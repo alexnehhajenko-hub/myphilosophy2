5b5bdcf27fc7e7768e10f9eaa158d1923c007b31
alexnehhajenko-hub <alexnehhajenko@gmail.com>
2025-11-08 18:05:54 +0100

    Initialize MyPhilosophy2App with AppState

--- MyPhilosophy2App.swift
+++ MyPhilosophy2App.swift
@@ -1,1 +1,14 @@
+import SwiftUI
+import Combine
 
+@main
+struct MyPhilosophy2App: App {
+    @StateObject private var appState = AppState()
+
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+                .environmentObject(appState)
+        }
+    }
+}
